b60bd35cb627899b4ab1edf3da0b88a95410a949
Juan Ignacio López <lopez.jignacio@gmail.com>
2023-01-30 21:26:44 -0300

    Front End Proyecto final

---
+++ js/cv.js
@@ -0,0 +1,80 @@
+/*const texto1 = document.getElementById('texto1');
+const texto2 = document.getElementById('texto2');
+const texto3 = document.getElementById('texto3');
+
+function mostrarTexto1(texto1){
+	if (texto1.style.display == ''){
+		texto1.style.display = 'none';
+	}else{
+		texto1.style.display = '';
+	}
+}
+
+function mostrarTexto2(texto2){
+	if (texto2.style.display == ''){
+		texto2.style.display = 'none';
+	}else{
+		texto2.style.display = '';
+	}
+}
+
+function mostrarTexto3(texto3){
+	if (texto3.style.display == ''){
+		texto3.style.display = 'none';
+	}else{
+		texto3.style.display = '';
+	}
+}
+
+
+
+    		const items = document.getElementsByClassName('item');
+    		function cambiarVisibilidad(numItem) {
+    			const item = items[numItem];
+    			if (item.style.visibility == '') {
+    				item.style.visibility = 'hidden';
+    			} else {
+    				item.style.visibility = '';
+    			}
+    		}
+
+    		function cambiarDisplay(numItem) {
+    			const item = items[numItem];
+    			if (item.style.display == '') {
+    				item.style.display = 'none';
+    			} else {
+    				item.style.display = '';
+    			}
+    		}
+
+*/    		
+
+function ocultar1(){
+document.getElementById('item1').style.display = 'none';
+}
+
+function mostrar1(){
+document.getElementById('item1').style.display = 'block';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
